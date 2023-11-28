56913d6972b34328e19c1db5706bb0d743833d0e
sofivasa <sofivasa@myuax.com>
2023-11-28 12:18:27 +0100

    Update ejerciciointegrador.cpp

--- ejerciciointegrador.cpp
+++ ejerciciointegrador.cpp
@@ -4,4 +4,19 @@
 #include "json11.hpp"
 #include "jsonlib.hpp"
 
-
+{
+"name": "RapidJSON",
+"version": "1.1.0",
+"keywords": "json, sax, dom, parser, generator",
+"description": "A fast JSON parser/generator for C++ with both SAX/DOM style API",
+"export": {
+"include": "include"
+},
+"examples": "example/*/*.cpp",
+"repository":
+{
+"type": "git",
+"url": "https://github.com/Tencent/rapidjson"
+}
+}
+enum variant_type { Symbol, Number, List, Proc, Lambda, Cadena };
